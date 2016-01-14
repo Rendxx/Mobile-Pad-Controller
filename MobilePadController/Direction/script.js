9476d3fc12bcd4bf25383636c0deec6561a6a3e7
Rendxx <rendxx@gmail.com>
2016-01-14 12:14:03 -0600

    Basic version is complete

--- MobilePadController/Direction/script.js
+++ MobilePadController/Direction/script.js
@@ -3,6 +3,20 @@ window.Rendxx.Game = window.Rendxx.Game || {};
 window.Rendxx.Game.Client = window.Rendxx.Game.Client || {};
 window.Rendxx.Game.Client.Controller = window.Rendxx.Game.Client.Controller || {};
 
+/*
+ * Controller.Direction
+ * This is a handler can move within a circle. 
+ * Support only 1 touch point
+ * Output the offset from center in 2 format (x,y / degree,strength)
+ * Output: 
+ * {
+ *      x: [int]            (0-100)
+ *      y: [int]            (0-100)
+ *      degree: [degree]    (0-360, clockwise)
+ *      strength: [int]     (0-100)
+ * }
+ */
+
 (function (Controller) {
     var HTML = {
         wrap: '<div class="controller-direction"></div>',
@@ -13,54 +27,129 @@ window.Rendxx.Game.Client.Controller = window.Rendxx.Game.Client.Controller || {
     var Direction = function (opts) {
         // private property
         var that = this,
-            pos_top = null,
-            pos_bottom = null,
-            pos_left = null,
-            pos_right = null,
+            // parameters
+            _css = null,
+            // html
             html_container = null,
             html_wrap = null,
             html_base = null,
-            html_handler = null;
+            html_handler = null,
+            // data
+            radius = null,
+            range = null,
+            offset_x = null,
+            offset_y = null,
+            // flag
+            enabled = false,
+            using = false;
 
-        // function
-        var _bindFunc = function () {
+        // callback
+        this.onChange = null;
 
+        // public function
+        this.show = function (opts) {
+            if (opts != null) _setOpts(opts);
+            html_wrap.show();
+
+            var rect = html_wrap[0].getBoundingClientRect()
+            offset_x = rect.left;
+            offset_y = rect.top;
+            enabled = true;
+        };
+
+        this.hide = function () {
+            enabled = false;
+            html_wrap.hide();
+            if (using) move(0,0);
+            using = false;
+        };
+
+        // private function
+        var output = function (x, y, strength, degree){
+            html_handler.css({
+                'left': x + 'px',
+                'top': -y + 'px'
+            });
+
+            if (that.onChange != null) that.onChange({
+                x: x,
+                y: y,
+                strength: strength,
+                degree: degree
+            });
+        };
+
+        var move = function (x, y) {
+            if (x==0 && y==0){
+                output(0, 0, 0, 0);
+                return;
+            }
+            x -= radius;
+            y = radius-y;
+            var strength = Math.sqrt(x * x + y * y);
+            var degree = Math.asin(x / strength);
+            if (strength > range) {
+                x = x / strength * range;
+                y = y / strength * range;
+                strength = range;
+            }
+            output(x, y, strength, degree);
         };
 
         // setup
+        var _setupFunc = function () {
+            html_wrap[0].addEventListener('touchstart', function (e) {
+                if (!enabled) return;
+                //var touch = event.changedTouches[0];
+            }, false);
+
+            html_wrap[0].addEventListener('touchmove', function (e) {
+                if (!enabled) return;
+                using = true;
+                var touch = event.changedTouches[0];
+                move(touch.clientX - offset_x, touch.clientY - offset_y);
+            }, false);
+
+            html_wrap[0].addEventListener('touchend', function (e) {
+                if (!enabled) return;
+                using = false;
+                move(0, 0);
+            }, false);
+        };
+
         var _setupHtml = function () {
             // html
             html_wrap = $(HTML.wrap).appendTo(html_container);
             html_base = $(HTML.base).appendTo(html_wrap);
             html_handler = $(HTML.handler).appendTo(html_wrap);
 
-            // css
-            html_wrap.css({
-                'top': pos_top === null ? 'auto' : pos_top,
-                'bottom': pos_bottom === null ? 'auto' : pos_bottom,
-                'left': pos_left === null ? 'auto' : pos_left,
-                'right': pos_right === null ? 'auto' : pos_right,
-            });
+            // data
             var handler_size = html_handler.width();
             var base_size = html_base.width();
+            radius = base_size/2;
+            range = (base_size - handler_size) / 2;
+
+            // css
+            html_wrap.css(_css);
             html_handler.css({
-                'margin-top': (base_size - handler_size) / 2,
-                'margin-left': (base_size - handler_size) / 2
-            })
+                'margin-top': range + 'px',
+                'margin-left': range + 'px'
+            });
+        };
+
+        var _setOpts = function (opts) {
+            if (opts.css) _css = opts.css;
         };
 
         var _init = function (opts) {
             if (opts == null) throw new Error("Option can not be empty");
             html_container = opts.container;
-            if (opts.top) pos_top = opts.top;
-            if (opts.bottom) pos_bottom = opts.bottom;
-            if (opts.left) pos_left = opts.left;
-            if (opts.right) pos_right = opts.right;
-
+            _setOpts(opts);
             _setupHtml();
-
+            _setupFunc();
+            that.hide();
         };
-        _init();
+        _init(opts);
     };
     Controller.Direction = Direction;
 })(window.Rendxx.Game.Client.Controller);
